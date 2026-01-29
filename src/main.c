f925730858fb9ce2b23f6aa652c1ecb26cdb200a
Penaz <penazarea@altervista.org>
2026-01-29 21:18:01 +0100

    Use is_run_started for autosplitter warnings too

--- src/main.c
+++ src/main.c
@@ -465,7 +465,7 @@ static void open_auto_splitter(GSimpleAction* action,
     } else {
         win = ls_app_window_new(LS_APP(app));
     }
-    if (win->timer->running) {
+    if (is_run_started(win->timer)) {
         GtkWidget* warning = gtk_message_dialog_new(
             GTK_WINDOW(win),
             GTK_DIALOG_MODAL,
