LibreSplit/LibreSplit

commit 58fefe904e79548ef514a819d3a46f53e939820a

Prompt user when resetting a run with a gold split (#253)
* Create confirm reset dialog

* Add detection logic for a gold in the current run

* Prompt for confirmation when run has a gold split

* Switch to using stdbool

* Move reset logic to not interfere with other logic

src/timer.c
@@ -3,6 +3,7 @@
  * Implementation of the timer
  */
 #include "timer.h"
+#include "gui/dialogs.h"
 #include "settings/utils.h"
 
 #include "lasr/auto-splitter.h"
@@ -475,6 +476,21 @@ void ls_game_update_bests(const ls_game* game,
     }
 }
 
+bool ls_timer_has_gold_split(const ls_timer* timer)
+{
+    if (!timer || !timer->split_info)
+        return false;
+
+    // Only consider splits that happened this run
+    const int committed = timer->curr_split;
+    for (int i = 0; i < committed; i++) {
+        if (timer->split_info[i] & LS_INFO_BEST_SEGMENT) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int ls_game_save(const ls_game* game)
 {
     int error = 0;
@@ -926,7 +942,15 @@ int ls_timer_reset(ls_timer* timer)
         if (timer->curr_split < timer->game->split_count) {
             ls_run_save(timer, "RESET");
         }
-        reset_timer(timer);
+        if (ls_timer_has_gold_split(timer)) {
+            bool user_reset = display_confirm_reset_dialog();
+            if (user_reset) {
+                reset_timer(timer);
+                return 1;
+            } else {
+                return 0;
+            }
+        }
         return 1;
     }
     return 0;

src/gui/dialogs.c
@@ -9,6 +9,7 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <stdatomic.h>
+#include <stdbool.h>
 
 /**
  * Opens the default browser on the LibreSplit troubleshooting documentation.
@@ -54,4 +55,20 @@ gboolean display_non_capable_mem_read_dialog(gpointer data)
 
     // Connect the response signal to the callback function
     return FALSE; // False removes this function from the queue
+}
+
+bool display_confirm_reset_dialog()
+{
+    GtkWidget* dialog = gtk_message_dialog_new(
+        NULL,
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_WARNING,
+        GTK_BUTTONS_YES_NO,
+        "This run contains a gold split.\n\n"
+        "Are you sure you want to reset?");
+    gtk_window_set_title(GTK_WINDOW(dialog), "Confirm Reset?");
+
+    gint response = gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    return response == GTK_RESPONSE_YES;
 }

src/gui/dialogs.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #include <glib.h>
+#include <stdbool.h>
 
 gboolean display_non_capable_mem_read_dialog(gpointer data);
+
+bool display_confirm_reset_dialog();

src/timer.h
@@ -70,6 +70,8 @@ void ls_game_update_splits(ls_game* game, const ls_timer* timer);
 
 void ls_game_update_bests(const ls_game* game, const ls_timer* timer);
 
+bool ls_timer_has_gold_split(const ls_timer* timer);
+
 int ls_game_save(const ls_game* game);
 
 void ls_game_release(const ls_game* game);

commit c215c9d9bdadb07c1569f20f5b8bcd401c0c5983

Fix timer not resetting in non-gold runs
Bug introduced in #253

src/timer.c
@@ -951,6 +951,7 @@ int ls_timer_reset(ls_timer* timer)
                 return 0;
             }
         }
+        reset_timer(timer);
         return 1;
     }
     return 0;