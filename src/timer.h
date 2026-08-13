521180f0ebe1379e0097fa7422cae2423264c258
Luna <starlit.luna.eve@gmail.com>
2026-08-13 19:04:48 -0400

    Add missing declaration.

--- src/timer.h
+++ src/timer.h
@@ -93,7 +93,9 @@ ls_time ls_time_subtract(ls_time a, ls_time b);
 
 long long ls_segment_value(long long current, long long previous, bool is_first_split);
 
-inline long long ls_time_get_by_method(ls_time time, ls_time_method method);
+long long ls_time_get_by_method(ls_time time, ls_time_method method);
+
+bool is_time_valid(long long time);
 
 long long ls_sum_of_bests(const ls_timer* timer, ls_time_method method);
 
