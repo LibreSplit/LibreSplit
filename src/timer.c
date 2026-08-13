eedaf5dad64dcd6a3a85b299255fd4c5ff58cf3c
Luna <starlit.luna.eve@gmail.com>
2026-08-13 07:19:07 -0400

    Format.

--- src/timer.c
+++ src/timer.c
@@ -737,14 +737,14 @@ int ls_run_save(ls_timer* timer, const char* reason)
             // Check if time > 0, avoids saving time on skipped splits
             if (
                 timer->split_times[i].game_time > 0 && timer->split_times[i].real_time > 0
-                    && timer->split_times[i].game_time < LLONG_MAX && timer->split_times[i].real_time < LLONG_MAX) {
+                && timer->split_times[i].game_time < LLONG_MAX && timer->split_times[i].real_time < LLONG_MAX) {
                 json_t* time = json_object();
                 json_time_set(time, &timer->split_times[i]);
                 json_object_set_new(split, "time", time);
                 // Check if segment time > 0, avoids saving segment time AFTER skipped split
                 if (
                     timer->segment_times[i].game_time > 0 && timer->segment_times[i].real_time > 0
-                        && timer->segment_times[i].game_time < LLONG_MAX && timer->segment_times[i].real_time < LLONG_MAX) {
+                    && timer->segment_times[i].game_time < LLONG_MAX && timer->segment_times[i].real_time < LLONG_MAX) {
                     json_t* segment = json_object();
                     json_time_set(segment, &timer->segment_times[i]);
                     json_object_set_new(split, "segment", segment);
