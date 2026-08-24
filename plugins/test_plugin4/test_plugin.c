b94e6968e26b3b41baeaba1622afc1347d73b1e9
Penaz <penazarea@altervista.org>
2026-08-24 13:30:51 +0200

    Update Context Menu Example

--- plugins/test_plugin4/test_plugin.c
+++ plugins/test_plugin4/test_plugin.c
@@ -1,3 +1,4 @@
+#include "glib-object.h"
 #include "plugins/plugin.h"
 #include "plugins/plugin_utils.h"
 #include <gtk/gtk.h>
@@ -8,10 +9,23 @@ const char plugin_description[] = "Does something: just registers a fake plugin 
 const char plugin_version[] = "0.1";
 const char plugin_author[] = "The LibreSplit Core Team";
 
+void show_plugin_dialog(GSimpleAction* action, GVariant* parameter, gpointer app)
+{
+    GtkWidget* dialog = gtk_message_dialog_new(
+        NULL,
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_INFO,
+        GTK_BUTTONS_OK,
+        "This is a dialog from a plugin");
+    gtk_window_set_title(GTK_WINDOW(dialog), "Just a dialog");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+}
+
 int register_context_menu(GtkWidget* parent)
 {
     GtkWidget* menu = gtk_menu_item_new_with_label("Plugin 4");
     gtk_menu_shell_append(GTK_MENU_SHELL(parent), menu);
+    g_signal_connect(menu, "activate", G_CALLBACK(show_plugin_dialog), NULL);
     return 0;
 }
 
