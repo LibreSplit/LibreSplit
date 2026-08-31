d6f3eaab5e0786105e6ecbdd4600eebfe6ad9eb4
Luna <starlit.luna.eve@gmail.com>
2026-08-31 13:44:02 -0400

    Remove accidental settings test commit. Finish alert widget implementation.

--- src/gui/widgets/alert.c
+++ src/gui/widgets/alert.c
@@ -1,8 +1,69 @@
 #include "alert.h"
-#include "dialog.h"
 
-// TODO: WIP Alets, but submitting this PR without finishing this in case we don't want them
-void ls_alert_open(GtkWindow* parent, const char* title, const char* message, const char* detail)
+/**
+ * @brief Presents an error alert to the user
+ *
+ * @param parent The parent window for this alert
+ * @param title The alert title that appears on the titlebar
+ * @param message The message header
+ * @param detail Optional message details
+ */
+void ls_alert_error(GtkWindow* parent, const char* title, const char* message, const char* detail)
+{
+    const LSDialogIcon icon = {
+        .source = "dialog-error",
+        .type = LS_DIALOG_ICON_NAME,
+    };
+
+    ls_alert(parent, title, message, detail, &icon);
+}
+
+/**
+ * @brief Presents a warning alert to the user
+ *
+ * @param parent The parent window for this alert
+ * @param title The alert title that appears on the titlebar
+ * @param message The message header
+ * @param detail Optional message details
+ */
+void ls_alert_warning(GtkWindow* parent, const char* title, const char* message, const char* detail)
+{
+    const LSDialogIcon icon = {
+        .source = "dialog-warning",
+        .type = LS_DIALOG_ICON_NAME,
+    };
+
+    ls_alert(parent, title, message, detail, &icon);
+}
+
+/**
+ * @brief Presents an informational alert to the user
+ *
+ * @param parent The parent window for this alert
+ * @param title The alert title that appears on the titlebar
+ * @param message The message header
+ * @param detail Optional message details
+ */
+void ls_alert_info(GtkWindow* parent, const char* title, const char* message, const char* detail)
+{
+    const LSDialogIcon icon = {
+        .source = "dialog-information",
+        .type = LS_DIALOG_ICON_NAME,
+    };
+
+    ls_alert(parent, title, message, detail, &icon);
+}
+
+/**
+ * @brief Create an alert-style window with any or no icon
+ *
+ * @param parent The parent window for this alert
+ * @param title The alert title that appears on the titlebar
+ * @param message The message header
+ * @param detail Optional message details
+ * @param icon Optional icon to display on the alert
+ */
+void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, LSDialogIcon* icon)
 {
     const LSDialogOption options[] = {
         {
@@ -13,10 +74,5 @@ void ls_alert_open(GtkWindow* parent, const char* title, const char* message, co
         }
     };
 
-    const LSDialogIcon icon = {
-        .source = "dialog-information",
-        .type = LS_DIALOG_ICON_NAME,
-    };
-
-    ls_dialog_open(parent, title, message, detail, options, &icon, G_N_ELEMENTS(options), NULL, NULL);
+    ls_dialog_open(parent, title, message, detail, options, icon, G_N_ELEMENTS(options), NULL, NULL);
 }
--- src/gui/widgets/alert.h
+++ src/gui/widgets/alert.h
@@ -1,5 +1,8 @@
 #pragma once
 
-#include <gtk/gtk.h>
+#include "dialog.h"
 
-void ls_alert_open(GtkWindow* parent, const char* title, const char* message, const char* detail);
+void ls_alert_error(GtkWindow* parent, const char* title, const char* message, const char* detail);
+void ls_alert_warning(GtkWindow* parent, const char* title, const char* message, const char* detail);
+void ls_alert_info(GtkWindow* parent, const char* title, const char* message, const char* detail);
+void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, LSDialogIcon* icon);
