02305d1d80dfb238f108f1012f38af1be7426326
Luna <starlit.luna.eve@gmail.com>
2026-08-31 15:32:12 -0400

    Make alert icon const.

--- src/gui/widgets/alert.c
+++ src/gui/widgets/alert.c
@@ -63,7 +63,7 @@ void ls_alert_info(GtkWindow* parent, const char* title, const char* message, co
  * @param detail Optional message details
  * @param icon Optional icon to display on the alert
  */
-void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, LSDialogIcon* icon)
+void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, const LSDialogIcon* icon)
 {
     const LSDialogOption options[] = {
         {
--- src/gui/widgets/alert.h
+++ src/gui/widgets/alert.h
@@ -5,4 +5,4 @@
 void ls_alert_error(GtkWindow* parent, const char* title, const char* message, const char* detail);
 void ls_alert_warning(GtkWindow* parent, const char* title, const char* message, const char* detail);
 void ls_alert_info(GtkWindow* parent, const char* title, const char* message, const char* detail);
-void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, LSDialogIcon* icon);
+void ls_alert(GtkWindow* parent, const char* title, const char* message, const char* detail, const LSDialogIcon* icon);
