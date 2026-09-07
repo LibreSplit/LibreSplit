aaf81e50c17f49af79a37da98c2478aa09bcdd92
Luna <starlit.luna.eve@gmail.com>
2026-09-07 16:17:04 -0400

    move gesture registration to ls_app_window_new.

--- src/gui/app_window.c
+++ src/gui/app_window.c
@@ -94,6 +94,13 @@ LSAppWindow* ls_app_window_new(LSApp* app)
     LOG_DEBUG("Creating a new LibreSplit window");
     LSAppWindow* win;
     win = g_object_new(LS_APP_WINDOW_TYPE, "application", app, NULL);
+    GtkGesture* click = gtk_gesture_click_new();
+    gtk_gesture_single_set_button(GTK_GESTURE_SINGLE(click), 0);
+    gtk_event_controller_set_propagation_phase(GTK_EVENT_CONTROLLER(click),
+        GTK_PHASE_CAPTURE);
+
+    g_signal_connect(click, "pressed", G_CALLBACK(handle_button_pressed), app);
+    gtk_widget_add_controller(GTK_WIDGET(win), GTK_EVENT_CONTROLLER(click));
     return win;
 }
 
@@ -124,17 +131,6 @@ void ls_app_window_open(LSAppWindow* win, const char* file)
     } else {
         ls_app_window_show_game(win);
     }
-}
-
-static void activate_gestures(GApplication* app, LSAppWindow* win)
-{
-    GtkGesture* click = gtk_gesture_click_new();
-    gtk_gesture_single_set_button(GTK_GESTURE_SINGLE(click), 0);
-    gtk_event_controller_set_propagation_phase(GTK_EVENT_CONTROLLER(click),
-        GTK_PHASE_CAPTURE);
-
-    g_signal_connect(click, "pressed", G_CALLBACK(handle_button_pressed), app);
-    gtk_widget_add_controller(GTK_WIDGET(win), GTK_EVENT_CONTROLLER(click));
 }
 
 /**
@@ -182,7 +178,6 @@ void ls_app_activate(GApplication* app)
         }
     }
     atomic_store(&auto_splitter_enabled, cfg.libresplit.auto_splitter_enabled.value.b);
-    activate_gestures(app, win);
 }
 
 void ls_app_open(GApplication* app,
@@ -205,7 +200,6 @@ void ls_app_open(GApplication* app,
         }
     }
 
-    activate_gestures(app, win);
     gtk_window_present(GTK_WINDOW(win));
 }
 
