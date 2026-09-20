a8e02d5876d266cc1640c7aa9e82cde00fa797a3
Luna <starlit.luna.eve@gmail.com>
2026-09-20 08:39:33 -0400

    Fix comment.

--- src/lasr/settings.h
+++ src/lasr/settings.h
@@ -5,18 +5,15 @@
 
 /**
  * @brief Indicates the type of the value.
- * Since this gets stored in the json file, make sure
- * we explicitly define the values to ensure they never change.
  * Always append to this list, never change values.
  * Make sure new settings go before SETTING_INVALID
- * SETTING_INVALID should always just be the last value so
- * do not define its value.
+ * SETTING_INVALID should always just be the last value.
  */
 typedef enum SettingType {
-    SETTING_BOOLEAN = 0,
-    SETTING_INTEGER = 1,
-    SETTING_NUMBER = 2,
-    SETTING_STRING = 3,
+    SETTING_BOOLEAN,
+    SETTING_INTEGER,
+    SETTING_NUMBER,
+    SETTING_STRING,
     SETTING_INVALID, // Easy way to compare a value >= this is invalid. Keep this last
 } SettingType;
 
